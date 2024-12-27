7614f6a5f58ffafb08373996903e9713024f4ede
Rudransh Singh Tomar <trudranshsingh2003@gmail.com>
2024-12-27 21:41:23 +0530

    Updated Formatiing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
         eprintln!("Problem parsing arguments: {err}"); // standard library provides the eprintln! macro that prints to the standard error stream,
         process::exit(1)
     });
-    println!("String that we have to find {}", config.query);
+    println!("String that we have to find is : {}", config.query);
+    println!("File in which we are searching is : {}", config.file_path);
     if let Err(e) = mingrep::run(config){
         eprintln!("Application Error {e}"); // * WE are using eprintlin!() macro to print error to standarda error stream not to print it on output.txt which is for standard output
         process::exit(1)
