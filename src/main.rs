35244373ba5ccd5e677ef7528dbbacae26a23dbb
Rudransh Singh Tomar <trudranshsingh2003@gmail.com>
2024-12-27 23:13:47 +0530

    defined the arguments for the commands

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ use mingrep::Config;
 use clap :: {Arg, Command};
 
 fn main() {
+    let matches = Command::new("minigrep")
+        .version("1.1")
+        .author("Rudransh Singh Tomar <trudranshsingh2003@gmail.com>")
+        .about("Search for a string in a file")
+        .arg(
+            Arg::new("query")
+                .help("The string to search for")
+                .required(true),
+            
+        )
+        .arg(
+            Arg::new("file_path")
+                .help("The file to search in")
+                .required(true)
+        )
+        .arg(
+            Arg::new("ignore_case")
+                .long("ignore-case")
+                .help("Perform a case-insensitive search"),
+        )
+        .arg(
+            Arg::new("output")
+                .long("output")
+                .value_name("File")
+                .help("Write search results to a file"),
+        )
+        .get_matches();
+
     let args : Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}"); // standard library provides the eprintln! macro that prints to the standard error stream,
